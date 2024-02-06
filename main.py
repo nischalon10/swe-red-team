8f24301430b0c1e8eca5dd6c6ca55ac0198fce71
nischalon10 <nischalon10@gmail.com>
2024-02-05 20:59:19 -0500

    working

--- main.py
+++ main.py
@@ -1,28 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for
+from website import create_app
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('login.html')
-
-@app.route('/logged_in')
-def main():
-    return render_template('app.html')
-
-@app.route('/login', methods=['POST'])
-def login():
-    username = request.form.get('username')
-    password = request.form.get('password')
-    # Add your logic to check username and password here
-    return redirect(url_for('main'))
-
-@app.route('/signup', methods=['POST'])
-def signup():
-    username = request.form.get('username')
-    password = request.form.get('password')
-    # Add your logic to create a new user here
-    return redirect(url_for('main'))
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
