8bf6c95e0c14295abfe46492e3264dd4a18c3355
Perliamo <45945912+perliamo@users.noreply.github.com>
2024-02-11 12:46:05 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+# main.py
+
+from incollege import InCollegeApp
+
+def main():
+    app = InCollegeApp()
+    app.main_menu()  # Assuming you have a method called main_menu to display the main menu
+
+if __name__ == "__main__":
+    main()
