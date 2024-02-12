eb2ee7b796f3ef986722e692f694eab376635cb6
perliamo <lnourmax64@gmail.com>
2024-02-12 18:29:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from incollege import InCollegeApp
 
 def main():
     app = InCollegeApp()
-    app.main_menu()  # Assuming you have a method called main_menu to display the main menu
+    app.main_menu()
 
 if __name__ == "__main__":
     main()
